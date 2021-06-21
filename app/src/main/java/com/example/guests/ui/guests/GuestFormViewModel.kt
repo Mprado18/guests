59c021e8448064269f70f9d9475ee08286c93ccc
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-06-21 16:59:14 -0300

    adjust logic view model

--- app/src/main/java/com/example/guests/ui/guests/GuestFormViewModel.kt
+++ app/src/main/java/com/example/guests/ui/guests/GuestFormViewModel.kt
@@ -1,21 +1,24 @@
 package com.example.guests.ui.guests
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.guests.service.model.GuestModel
 import com.example.guests.service.repository.GuestRepository
 
-class GuestFormViewModel : ViewModel() {
+class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val mGuestRepository: GuestRepository = GuestRepository()
+    private val mContext = application.applicationContext
+    private val mGuestRepository: GuestRepository = GuestRepository.getInstance(mContext)
 
     private var mSaveGuest = MutableLiveData<Boolean>()
     val saveGuest: LiveData<Boolean> = mSaveGuest //será observado e não pode ser alterado de fora da ViewModel
 
     fun saveDataGuest(name: String, presence: Boolean) {
-        val guest = GuestModel(name, presence)
-        mGuestRepository.save(guest)
+        val guest = GuestModel(name = name, presence = presence)
+
+        mSaveGuest.value = mGuestRepository.save(guest)
     }
 
 }
