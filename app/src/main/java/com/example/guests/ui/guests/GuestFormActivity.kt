e5af4adbbf3c10f4f9848ace60bc6bb2bb6d75a4
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-06-23 15:54:57 -0300

    add observer and function into form activity

--- app/src/main/java/com/example/guests/ui/guests/GuestFormActivity.kt
+++ app/src/main/java/com/example/guests/ui/guests/GuestFormActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.guests.constants.GuestConstants
 import com.example.guests.databinding.ActivityGuestFormBinding
 
 class GuestFormActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGuestFormBinding
     private lateinit var mViewModel: GuestFormViewModel
+
+    private var mGuestId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,14 +25,24 @@ class GuestFormActivity : AppCompatActivity() {
 
         binding.buttonSaveGuestsData.setOnClickListener { setListeners() }
         observeGuest()
+        loadData()
+
+    }
+
+    //carrega dados do bundle
+    private fun loadData() {
+        val bundle = intent.extras
+        if (bundle != null) {
+            mGuestId = bundle.getInt(GuestConstants.GUESTID)
+            mViewModel.load(mGuestId)
+        }
     }
 
     private fun setListeners() {
-
         val name = binding.inputName.text.toString()
         val presence = binding.presence.isChecked
 
-        mViewModel.saveDataGuest(name, presence)
+        mViewModel.saveDataGuest(mGuestId, name, presence)
     }
 
     private fun observeGuest() {
@@ -40,6 +53,17 @@ class GuestFormActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(applicationContext, "Failed", Toast.LENGTH_SHORT).show()
                 finish()
+            }
+        })
+
+        mViewModel.guest.observe(this, Observer {
+            with(binding) {
+                inputName.setText(it.name)
+                if (it.presence) {
+                    presence.isChecked = true
+                } else {
+                    absent.isChecked = true
+                }
             }
         })
     }
