32657a2a6fb2ee8900285b213ad823b9e5a511c7
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-06-23 15:57:46 -0300

    add function save and load into form view model

--- app/src/main/java/com/example/guests/ui/guests/GuestFormViewModel.kt
+++ app/src/main/java/com/example/guests/ui/guests/GuestFormViewModel.kt
@@ -15,10 +15,21 @@ class GuestFormViewModel(application: Application) : AndroidViewModel(applicatio
     private var mSaveGuest = MutableLiveData<Boolean>()
     val saveGuest: LiveData<Boolean> = mSaveGuest //será observado e não pode ser alterado de fora da ViewModel
 
-    fun saveDataGuest(name: String, presence: Boolean) {
-        val guest = GuestModel(name = name, presence = presence)
+    private var mGuest = MutableLiveData<GuestModel>()
+    val guest: LiveData<GuestModel> = mGuest
 
-        mSaveGuest.value = mGuestRepository.save(guest)
+    fun saveDataGuest(id: Int, name: String, presence: Boolean) {
+        val guest = GuestModel(id, name, presence)
+
+        if (id == 0) {
+            mSaveGuest.value = mGuestRepository.save(guest)
+        } else {
+            mSaveGuest.value = mGuestRepository.update(guest)
+        }
+    }
+
+    fun load(id: Int) {
+        mGuest.value = mGuestRepository.get(id)
     }
 
 }
